b82058aa292a703221d1855e6c37cda9be3a0b28
dafontebfp <bruniko.pereira@gmail.com>
2025-05-04 14:34:13 -0300

    wip

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -1,5 +1,3 @@
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from product.models import Product
